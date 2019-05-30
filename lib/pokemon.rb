d00641be86fa6a9de75ebcb1d17ca40bb7d5abf7
Andy Feidje Joseph <ingandy75@gmail.com>
2019-05-30 14:29:28 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,5 @@
 class Pokemon
-  attr_accessor :id,:name,:type, :db
+  attr_accessor :id,:name,:type, :db, :hp
 
   def initialize(name)
     @name = name
@@ -10,6 +10,9 @@ class Pokemon
   end
 
   def self.find(id,database_connection)
-    database_connection.execute("SELECT name,type FROM pokemon WHERE ID = ?",id)
+    database_connection.execute("SELECT name,type FROM pokemon WHERE ID = ?",id)[0]
+    id_, name, type, hp = answer
+    
+    new(id: id_, name: name, type: type,db: db, hp: hp)
   end
 end
